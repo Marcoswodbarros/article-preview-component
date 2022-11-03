ae742d98942c8fd339d494e7f7801db3e4948c75
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2022-11-03 12:30:47 -0300

    Adding js folder

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let share = document.querySelector(".profile__shareIcon").addEventListener("click", function () {
+    document.querySelector(".sharing").style.display="flex";
+});
+
+let hide = document.querySelector(".sharing__icon").addEventListener("click", function () {
+    document.querySelector(".sharing").style.display="none";
+});
